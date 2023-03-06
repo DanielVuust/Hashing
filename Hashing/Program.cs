46345e4f64585b4cf4a6d5a19b25ed8c03995320
Daniel Nygaard Vuust28 <daniel.vuust@gmail.com>
2023-03-06 13:27:54 +0100

    Initial commit

---
+++ Hashing/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Hashing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        private static void DisplayInfo()
+        {
+            Console.Clear();
+            Console.WriteLine("Hash Type:");
+            Console.WriteLine();
+            Console.WriteLine("Key:");
+            Console.WriteLine();
+            Console.WriteLine("Plain text ASCII:");
+            Console.WriteLine();
+            Console.WriteLine("MAC ASCII: ");
+            Console.WriteLine("MAC HEX: ");
+
+
+        }
+    }
+}
